af59a7f648d8e14d50601c8144f566fc732b703f
Clissman J. Camacho <clissmancamacho@gmail.com>
2019-01-05 11:24:20 -0400

    Initial Commit

---
+++ build/precache-manifest.376ad230c5fc4c4b1b1b147b04b9d7c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7269ad048a5aa7c6bd23",
+    "url": "/static/css/main.4cdc716f.chunk.css"
+  },
+  {
+    "revision": "7269ad048a5aa7c6bd23",
+    "url": "/static/js/main.7269ad04.chunk.js"
+  },
+  {
+    "revision": "231fe7e71b39c2da6e66",
+    "url": "/static/js/1.231fe7e7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f7a1dab0579ea4645f9253dfb6eaf77",
+    "url": "/index.html"
+  }
+];
